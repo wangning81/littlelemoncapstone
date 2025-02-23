a855e115b5d18b2b669b0cfc93d0e7f853b644b4
Ning Wang <wangning81@gmail.com>
2025-02-22 22:08:04 -0800

    Added database

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
